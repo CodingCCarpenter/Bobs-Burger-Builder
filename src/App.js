3492e68b13d79c406161e8ef42bdb0acc26c3881
CodingCCarpenter <christineassists@aol.com>
2019-10-16 16:06:23 -0500

    change app.js file to class syntax for extra features

--- src/App.js
+++ src/App.js
@@ -1,34 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person.js';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Bob's Burger Builder</h1>
-      <p>Your burger, your way!</p>
-      <Person name='Max' age='28'>My Hobbies: Racing, Painting</Person>
-      <Person name='Manu' age='29' />
-      <Person name='Stephanie' age='26'/>
-    </div>
-  );
-}
-
-export default App;
-
-
-// import React, { Component } from 'react';
-// import './App.css';
-
-// class App extends Component {
-//   render() {
-//     return(
-//       <div className='App'>
-//         <h1>Bob's Burger Builder</h1>
-//       </div>
-//     );
+class App extends Component {
+  render() {
+    return(
+      <div className='App'>
+        <h1>Bob's Burger Builder</h1>
+        <p>Your burger, your way!</p>
+        <button>Switch Name</button>
+        <Person name='Max' age='28'>My Hobbies: Racing, Painting</Person>
+        <Person name='Manu' age='29' />
+        <Person name='Stephanie' age='26'/>
+     </div>
+   );
     
-// }
+ }
 
 
-
+ 
