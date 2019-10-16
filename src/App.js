0cb7562737d409a2c0e2fb894e700e61488c5d7f
CodingCCarpenter <christineassists@aol.com>
2019-10-16 15:43:57 -0500

    add Person folder and Person.js component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person.js';
 
 function App() {
   return (
     <div className="App">
       <h1>Bob's Burger Builder</h1>
+      <p>Your burger, your way!</p>
+      <Person />
     </div>
   );
 }
