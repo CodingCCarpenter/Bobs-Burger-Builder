4c064547a3bd99290997b3e42d115cbbabc2ea28
CodingCCarpenter <christineassists@aol.com>
2019-10-16 15:17:28 -0500

    add notes with class syntax example in app.js

--- src/App.js
+++ src/App.js
@@ -10,3 +10,20 @@ function App() {
 }
 
 export default App;
+
+
+// import React, { Component } from 'react';
+// import './App.css';
+
+// class App extends Component {
+//   render() {
+//     return(
+//       <div className='App'>
+//         <h1>Bob's Burger Builder</h1>
+//       </div>
+//     );
+    
+// }
+
+
+
